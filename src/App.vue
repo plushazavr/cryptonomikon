7db32511aa975b399b1e2f6dbfe68d34c06a8c1b
Ekaterina Kazantseva <dr.kazantseva.ekaterina@gmail.com>
2022-12-04 16:27:11 +0300

    фильтрация списка по наличию буквы

--- src/App.vue
+++ src/App.vue
@@ -61,9 +61,24 @@
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div>
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Назад
+          </button>
+          <button
+            class="my-4 mx-2 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+          >
+            Вперед
+          </button>
+          <div>Фильтр: <input v-model="filter" type="text" placeholder="Введите букву" class="block pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"/></div>
+        </div>
+
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <div 
-            v-for="t in tickers"
+            v-for="t in filteredTickers()"
             :key="t.name"
             @click="select(t)"
             :class="{
@@ -131,7 +146,9 @@ export default {
     return {
       ticker: '',
       tickers: [],
-      sel: null
+      sel: null,
+      page: 1,
+      filter: ''
     }
   }, 
 
@@ -147,6 +164,11 @@ export default {
   },
 
   methods: {
+    //возвращает отфильтрованный список тикеров по наличию введенных букв в инпут
+    filteredTickers() {
+      return this.tickers.filter(ticker => ticker.name.includes(this.filter.toUpperCase()))
+    },
+
     subscribeToUpdates(tickerName) {
       //соединение с api и получение-обновление данных через интервал времени
       setInterval(async () => {
@@ -159,8 +181,7 @@ export default {
         }
       }, 3000)
       this.ticker = ''
-    },
-  },
+    },  
 
     //добавление тикера и его графика
     add() {
@@ -170,6 +191,7 @@ export default {
         graph: []
       }
       this.tickers.push(currentTicker); //пушим новый тикер в конец массива
+      this.filter = ''; //сбрасываем фильтер после добавления нового тикера
 
       localStorage.setItem('cryptonomikon-list', JSON.stringify(this.tickers));
       this.subscribeToUpdates(currentTicker.name) //запись в локальное хранилище и данные преобразуем в JSON
@@ -180,10 +202,12 @@ export default {
       this.sel = ticker;
       this.graph = [];
     },
+
     //удаление тикера
     handleDelete(tickerToRemove) {
       this.tickers = this.tickers.filter(t => t !== tickerToRemove)
     },
+
     //рисуем график с учетом мах и min высоты
     normalizeGraph() {
       const maxValue = Math.max(...this.graph);
@@ -192,6 +216,7 @@ export default {
         price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
     }
-  }
+  },
+};
 
 </script>
